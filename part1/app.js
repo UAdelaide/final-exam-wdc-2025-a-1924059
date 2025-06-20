52dff72dcc88e27bc1cb2ad075586cd548dd955d
a-1924059 <a1924059@adelaide.edu.au>
2025-06-20 15:45:43 +0000

    [automatic commit] 2025-06-20 15:45:43 UTC 115.69.5.195

--- part1/app.js
+++ part1/app.js
@@ -21,13 +21,17 @@ async function init() {
     `);
 
     await db.query(`
-      INSERT IGNORE INTO Dogs (owner_id, name, size)
-      VALUES (
-        (SELECT user_id FROM Users WHERE username = 'alice123'),
-        'Max',
-        'medium'
-      );
-    `);
+  INSERT INTO Dogs (owner_id, name, size)
+  SELECT * FROM (
+    SELECT (SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'
+  ) AS tmp
+  WHERE NOT EXISTS (
+    SELECT 1 FROM Dogs
+    WHERE name = 'Max'
+    AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')
+  );
+`);
+
 
     await db.query(`
       INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
