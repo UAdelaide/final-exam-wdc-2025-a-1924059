bd872dda888599eccc5b936c761c89844c18bff8
a-1924059 <a1924059@adelaide.edu.au>
2025-06-20 16:03:53 +0000

    [automatic commit] 2025-06-20 16:03:53 UTC 115.69.5.195

--- part1/app.js
+++ part1/app.js
@@ -109,6 +109,6 @@ app.get('/api/walkers/summary', async (req, res) => {
 
 init().then(() => {
   app.listen(port, () => {
-    console.log(`🚀 Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
   });
 });
