31e795e086a6944c1a474f9b1cc4a1043a4e28b8
a-1924059 <a1924059@adelaide.edu.au>
2025-06-21 03:31:08 +0000

    [automatic commit] 2025-06-21 03:31:07 UTC 129.127.202.67

--- part2/app.js
+++ part2/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session'); 
+const session = require('express-session');
 require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
