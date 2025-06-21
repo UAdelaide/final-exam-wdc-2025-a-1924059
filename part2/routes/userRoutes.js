061cf8e4db0fd0054b29271bd0a38505e422e64a
a-1924059 <a1924059@adelaide.edu.au>
2025-06-21 04:42:01 +0000

    [automatic commit] 2025-06-21 04:42:01 UTC 129.127.202.67

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -64,7 +64,9 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  req.session.destroy
+  req.session.destroy(() => {
+    res.clearCookie('connect.sid')
+  }
 }
 
 )
