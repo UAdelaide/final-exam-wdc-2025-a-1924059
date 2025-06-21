8c103a6f82703e4730dd2bbf8388bb9efa32a146
a-1924059 <a1924059@adelaide.edu.au>
2025-06-21 03:30:47 +0000

    [automatic commit] 2025-06-21 03:30:47 UTC 129.127.202.67

--- part2/app.js
+++ part2/app.js
@@ -1,26 +1,4 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session'); 
 require('dotenv').config();
-const session = require('express-session');
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
-
-// Export the app instead of listening here
-module.exports = app;
